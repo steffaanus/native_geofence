70337ad1641d38fb05b9d958b05351d51c99ca48
Steffaanus <s.cremers@cloudalert.eu>
2025-11-28 10:55:20 +0100

    callback dispatcher async

--- lib/src/callback_dispatcher.dart
+++ lib/src/callback_dispatcher.dart
@@ -4,12 +4,12 @@ import 'package:native_geofence/src/api/native_geofence_trigger_impl.dart';
 import 'package:native_geofence/src/native_geofence_background_manager.dart';
 
 @pragma('vm:entry-point')
-void callbackDispatcher() {
+Future<void> callbackDispatcher() async {
   debugPrint('Callback dispatcher called.');
   // Setup connection between platform and Flutter.
   WidgetsFlutterBinding.ensureInitialized();
   // Create the NativeGeofenceTriggerApi.
   NativeGeofenceTriggerImpl.ensureInitialized();
   // Create the NativeGeofenceBackgroundApi.
-  createNativeGeofenceBackgroundManagerInstance();
+  await createNativeGeofenceBackgroundManagerInstance();
 }
